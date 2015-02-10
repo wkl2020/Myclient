121d644bf1b89871c4bc640ddc611371cbb905dc
kailong.wu <wukailong_2009@163.com>
2015-02-10 15:26:11 +0800

    Add local weddav servlet

--- src/main/java/com/security/cloud/main/Main.java
+++ src/main/java/com/security/cloud/main/Main.java
@@ -1,11 +1,23 @@
 package com.security.cloud.main;
 
 import java.io.File;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import org.apache.catalina.startup.Tomcat;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        
+        Logger logger = Logger.getLogger("");
+        Handler fileHandler = new FileHandler("logs/catalina.out", true);
+        fileHandler.setFormatter(new SimpleFormatter());
+        fileHandler.setLevel(Level.ALL);
+        fileHandler.setEncoding("UTF-8");
+        logger.addHandler(fileHandler);
 
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
